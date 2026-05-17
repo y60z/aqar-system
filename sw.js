9456161d4684d6ae13d523d00815de146dc9d718
y60z <pvywtvmrrs@privaterelay.appleid.com>
2026-05-17 09:20:02 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'aqar-system-v2';
-const FILES = ['./','./index.html','./style.css','./app.js','./manifest.json','./logo.png','./watermark.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(()=>caches.match('./index.html')))));
